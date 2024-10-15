583c07c75c37d61ccbfeadea71b97e39c81e19ce
Freder Steven Hernandez Llaten <fredersteven399@hotmail.com>
2024-10-14 21:35:19 -0500

    fix: credential validation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @if (Auth::user()->has('credential')->exists())
+                    @if (Auth::user()->credential()->exists())
                         <message :closable="false">Keys Config</message>
                     @else
                         <access-key-form :regions=@json(App\Enums\AWSRegion::values())></access-key-form>

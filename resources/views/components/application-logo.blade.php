90d440e97c23146d0e2eb1eb2f4821f7a37e63db
Freder Steven Hernandez Llaten <fredersteven399@hotmail.com>
2024-10-26 16:38:48 -0500

    style: add extra icons

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <div id="logo-container" class="relative flex w-full flex-1 items-stretch">
-    <img src="images/logo-dark.svg" alt="logo-dark" {!! $attributes->merge(['class' => 'dark:hidden']) !!}>
-    <img src="images/logo-light.svg" alt="logo-light" {!! $attributes->merge(['class' => 'hidden dark:block']) !!}>
+    <img src="images/logo-dark.png" alt="logo-dark" {!! $attributes->merge(['class' => 'dark:hidden']) !!}>
+    <img src="images/logo-light.png" alt="logo-light" {!! $attributes->merge(['class' => 'hidden dark:block']) !!}>
 </div>
 
